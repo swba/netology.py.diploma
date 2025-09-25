74084e847acf115e754e4ae056d095740ece2f78
Stan <stan@kind.co>
2025-09-25 22:53:59 +0300

    Added a custom field PhoneField.

--- django/apps/base/models.py
+++ django/apps/base/models.py
@@ -1,5 +1,7 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
 class LoggableModel(models.Model):
@@ -34,3 +36,29 @@ class DeletableModel(models.Model):
             else:
                 self.deleted_at = None
             self.save(update_fields=['is_deleted', 'deleted_at'])
+
+
+class PhoneField(models.CharField):
+    """A phone field with validation."""
+
+    def __init__(
+            self,
+            *args,
+            max_length=14,
+            default='',
+            blank=True,
+            help_text=_("Phone number in international format: +79995550022"),
+            **kwargs
+        ):
+        super().__init__(*args, **kwargs)
+        self.max_length = max_length
+        self.default = default
+        self.blank = blank
+        self.help_text = help_text
+        self.validators = [RegexValidator(r'^\+[1-9]\d{10-12}$')]
+
+    @staticmethod
+    def format(value):
+        if value and len(value) >= 12:
+            return f"{value[:-10]} {value[-10:-7]} {value[-7:-4]}-{value[-4:]}"
+        return value
