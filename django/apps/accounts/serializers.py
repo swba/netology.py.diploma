a65486aedd24cef9b4e3acc8f62bd74f2459e182
Stan <stan@kind.co>
2025-09-27 18:58:06 +0300

    Updated UserSerializer to validate password and refine readonly and write-only fields.

--- django/apps/accounts/serializers.py
+++ django/apps/accounts/serializers.py
@@ -1,15 +1,30 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
 
 from apps.accounts.models import User
 
 
-class UserCreateSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    """User serializer."""
+
+    password = serializers.CharField(
+        max_length=128,
+        write_only=True)
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'first_name', 'last_name')
+        fields = ('id', 'email', 'password', 'first_name', 'last_name')
+        read_only_fields = ('id',)
 
-    # noinspection PyMethodMayBeStatic
-    def validate_password(self, value: str) -> str:
-        return make_password(value)
+    def validate(self, attrs):
+        # Validate user password using builtin Django validators.
+        password = attrs.get('password')
+        try:
+            validate_password(password, User(**attrs))
+        except ValidationError as e:
+            raise serializers.ValidationError({'password': e.messages})
+        # If password is OK, hash it.
+        attrs['password'] = make_password(password)
+        return attrs
